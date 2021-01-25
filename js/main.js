10a638b4cde77acc82cb35878ee07c0db5692bf1
Ваня и Лизочка <ivan18.12.92@mail.ru>
2021-01-25 21:37:26 +0700

    init

---
+++ js/main.js
@@ -0,0 +1,146 @@
+$(document).ready(() => {
+    let hairstyle = $('#hairstyle');
+    let decoration = $('#decoration');
+    let galleryHairstyle = $('#gallery-hairstyle');
+    let galleryDecoration = $('#gallery-decoration');
+
+    galleryHairstyle.slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        dots: false,
+        initialSlide: 0,
+        responsive: [
+            {
+                breakpoint: 1433,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    initialSlide: 0
+                }
+            },
+            {
+                breakpoint: 1023,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    initialSlide: 0
+                }
+            }
+        ]
+    });
+
+    galleryDecoration.slick({
+        infinite: true,
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        dots: false,
+        initialSlide: 0,
+        responsive: [
+            {
+                breakpoint: 1180,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    initialSlide: 0
+                }
+            }
+        ]
+    });
+
+    hairstyle.click((e) => {
+        galleryHairstyle.css('display', 'block');
+        galleryDecoration.css('display', 'none');
+        hairstyle.css('border-bottom', '7px solid #00204C');
+        decoration.css('border-bottom', '2px solid #BEC9CA');
+    });
+
+    decoration.click((e) => {
+        galleryDecoration.css('display', 'block');
+        galleryHairstyle.css('display', 'none');
+        hairstyle.css('border-bottom', '2px solid #BEC9CA');
+        decoration.css('border-bottom', '7px solid #00204C');
+    });
+
+    $('#reviews-gallery').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: false,
+        initialSlide: 0
+    });
+
+    $('.open-modal').click((e) => {
+        $('#form').css('display', 'flex');
+    });
+    $('#form').click((e) => {
+        if (e.target.id === 'form') {
+            $('#form').hide();
+        }
+    });
+    $('.close').click((e) => {
+        $('#form').hide();
+    });
+    $('#form-answer').click((e) => {
+        if (e.target.id === 'form') {
+            $('#form').hide();
+        }
+    });
+
+    $('#reserved-button').on('click', function () {
+        let name = $('#name');
+        let phone = $('#form-phone');
+        let hasError = false;
+        name.attr("placeholder", 'Ваше имя*');
+        phone.attr("placeholder", 'Ваш телефон*');
+        $('.input').css('borderColor', 'rgb(255, 255, 255)');
+
+        if (!name.val()) {
+            name.css('borderColor', 'red');
+            name.attr("placeholder", 'Введите ваше имя*');
+            hasError = true;
+        }
+
+        if (!phone.val()) {
+            phone.css('borderColor', 'red');
+            phone.attr("placeholder", 'Введите ваш телефон*');
+            hasError = true;
+        }
+
+        if (!hasError) {
+            $('#form-answer').css('display', 'block');
+            $('#form-container').css('display', 'none');
+            /*
+            $.ajax({
+                method: "POST",
+                url: '',
+                data: {name: name.val(), phone: phone.val(), procedure: procedure.val(), time: time.val()}
+            })
+                .done(function (message) {
+                    console.log(message);
+                    if (message.success) {
+                        $('form').replaceWith( "<div id='newDiv'>Спасибо за вашу заявку. Мы скоро свяжемся с Вами!</div>" );
+                    } else {
+                        $('form').replaceWith( "<div id='newDiv'>Возникла ошибка, позвоните нам и запишитесь по телефону.</div>" );
+                    }
+                }); */
+        }
+    })
+
+    $('#burger').click((e) => {
+        $('#header #menu').css('display', 'flex');
+    });
+
+    $('#header #menu .menu-item, #header-close').click((e) => {
+        $('#header #menu').css('display', 'none');
+    });
+
+    $('#header #menu').click((e) => {
+        if (e.target.id === 'menu') {
+            $('#header #menu').hide();
+        }
+    });
+});

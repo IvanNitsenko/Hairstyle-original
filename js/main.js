7b4b6feb2dbf65a003f537a7ce77bae115da7c30
IvanNitsenko <74192324+IvanNitsenko@users.noreply.github.com>
2021-01-31 17:55:09 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -76,6 +76,84 @@ $(document).ready(() => {
     $('.open-modal').click((e) => {
         $('#form').css('display', 'flex');
     });
+$(document).ready(() => {
+    let hairstyle = $('#hairstyle');
+    let decoration = $('#decoration');
+    let galleryHairstyle = $('#gallery-hairstyle');
+    let galleryDecoration = $('#gallery-decoration');
+
+    galleryHairstyle.slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        dots: false,
+        initialSlide: 0,
+        responsive: [
+            {
+                breakpoint: 1433,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    initialSlide: 0
+                }
+            },
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    initialSlide: 0
+                }
+            }
+        ]
+    });
+
+    galleryDecoration.slick({
+        infinite: true,
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        dots: false,
+        initialSlide: 0,
+        responsive: [
+            {
+                breakpoint: 1180,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    initialSlide: 0
+                }
+            }
+        ]
+    });
+
+    hairstyle.click((e) => {
+        galleryHairstyle.css('display', 'block');
+        galleryDecoration.css('display', 'none');
+        hairstyle.css('border-bottom', '7px solid #00204C');
+        decoration.css('border-bottom', '2px solid #BEC9CA');
+    });
+
+    decoration.click((e) => {
+        galleryDecoration.css('display', 'block');
+        galleryHairstyle.css('display', 'none');
+        hairstyle.css('border-bottom', '2px solid #BEC9CA');
+        decoration.css('border-bottom', '7px solid #00204C');
+    });
+
+    $('#reviews-gallery').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: false,
+        initialSlide: 0
+    });
+
+    $('.open-modal').click((e) => {
+        $('#form').css('display', 'flex');
+    });
     $('#form').click((e) => {
         if (e.target.id === 'form') {
             $('#form').hide();
@@ -130,17 +208,31 @@ $(document).ready(() => {
         }
     })
 
-    $('#burger').click((e) => {
-        $('#header #menu').css('display', 'flex');
-    });
+    var widthWind = $(this).width();
+    let headerMenu = $('#header #menu');
 
-    $('#header #menu .menu-item, #header-close').click((e) => {
-        $('#header #menu').css('display', 'none');
-    });
 
-    $('#header #menu').click((e) => {
-        if (e.target.id === 'menu') {
-            $('#header #menu').hide();
+    $(window).resize(function (e) {
+        if (widthWind <= 767) {
+            $('#burger').click((e) => {
+                $('#header #menu').css('display', 'flex');
+            });
+
+            $('#header #menu .menu-item, #header-close').click((e) => {
+                $('#header #menu').css('display', 'none');
+            });
+
+            headerMenu.click((e) => {
+                if (e.target.id === 'menu') {
+                    $('#header #menu').hide();
+                }
+            });
         }
-    });
+    })
+
+    $(window).resize(function (e) {
+        if (widthWind <= 768) {
+            headerMenu.css('display', 'flex');
+        }
+    })
 });
